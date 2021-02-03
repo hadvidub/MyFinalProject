40941acf9be143f5b1f2f36b396e866a4bbf4cce
Mu Tu <hadvidub@gmail.com>
2021-02-03 23:46:18 +0300

    3 Şubat Ders Sonu

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
-{
-    public interface IEntityRepository<T>
+{   //where T: class diyince bu referans tip olması gerekiyor demek istedik yani int, book falan olmaz demek istedik. 
+    //Sonrasında da IEntity yada ona implente olan bir referans tip olması gerekir demek istedik ama IEntity'nin kendisini istemiyoruz aslında
+    // new () : new'lenebilir bir nesne olmalı. IEntity newlenemyeceği için o kullanılamayacak
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
-        List<T> GetAll();
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-
-        List<T> GetAllByCategory(int categoryId);
     }
 }
